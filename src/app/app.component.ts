73b2f9ea8e0ccb320dbce25df21598db3da81a53
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2025-02-25 01:32:32 +0300

    done route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { InventoryComponent } from './pages/inventory/inventory.component';
 import { CustomerComponent } from './pages/customer/customer.component';
 import { BillComponent } from './pages/bill/bill.component';
@@ -7,7 +7,7 @@ import { BillComponent } from './pages/bill/bill.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

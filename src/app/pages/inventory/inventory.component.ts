bead025f8cf427aee0bb964ea1ec6a9ff4d78ca1
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2025-02-27 00:47:16 +0300

    with api

--- src/app/pages/inventory/inventory.component.ts
+++ src/app/pages/inventory/inventory.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inventory',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.css'
 })
 export class InventoryComponent {
 
+httpClient =inject(HttpClient);
+
+  inventoryData = {
+    productId:"",
+    productName:"",
+    stockAvailble:0,
+    reorderStock:0
+  }
+
+  onSubmit():void {
+    let apiUrl = "https://localhost:7104/api/Inventory";
+    let httpOptions = {
+      headers: new HttpHeaders({
+        Authorization:'TestCodeiz-auth-token',
+        'Content-Type' : 'application/json'
+      })
+    }
+    this.httpClient.post(apiUrl,this.inventoryData,httpOptions).subscribe((result) => {
+      alert('Form Submitted'+ JSON.stringify(this.inventoryData));
+
+    })
+  }
 }

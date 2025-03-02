6ebdcbf5598fd7397383e94862024566243139ce
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2025-03-03 00:11:00 +0300

    add customer page and layout and add NgbModal

---
+++ src/app/pages/customer-detail-dialog/customer-detail-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-detail-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-detail-dialog.component.html',
+  styleUrl: './customer-detail-dialog.component.css'
+})
+export class CustomerDetailDialogComponent {
+
+}
--- src/app/pages/customer/customer.component.ts
+++ src/app/pages/customer/customer.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CustomerDetailDialogComponent } from '../customer-detail-dialog/customer-detail-dialog.component';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css'
 })
 export class CustomerComponent {
+private modelService = inject(NgbModal);
 
+openModal(){
+  this.modelService.open(CustomerDetailDialogComponent);
+}
 }
